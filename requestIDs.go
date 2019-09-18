2b622f8063de4fef9dbb9ea2b906724709d55381
mishuyuan <mishuyuan@woda.ink>
2019-09-18 21:00:59 +0800

    add store context for goroutine

--- requestIDs.go
+++ requestIDs.go
@@ -1,26 +1,35 @@
 package log15
 
 import (
+	"context"
 	"github.com/petermattis/goid"
 	"sync"
 )
 
+type storeMeta struct  {
+	reqID interface{}
+	reqContext context.Context
+}
+
 var (
-	requestIDs = map[int64]interface{}{}
+	requestIDs = map[int64]storeMeta{}
 	rwm        sync.RWMutex
 )
 
-// Set 保存一个 RequestID
-func SetReqIDForGoroutine(ID interface{}) {
+// Set 保存一个 RequestID, context
+func SetReqMetaForGoroutine(ctx context.Context, ID interface{}) {
 	goID := getGoID()
 	rwm.Lock()
 	defer rwm.Unlock()
 
-	requestIDs[goID] = ID
+	requestIDs[goID] = storeMeta {
+		reqID:ID,
+		reqContext:ctx,
+	}
 }
 
-// Get 返回设置的 RequestID
-func GetReqIDForGoroutine() (interface{},bool) {
+// Get 返回设置的 ReqMeta
+func getReqMetaForGoroutine() (interface{},bool) {
 	goID := getGoID()
 	rwm.RLock()
 	defer rwm.RUnlock()
@@ -30,8 +39,26 @@ func GetReqIDForGoroutine() (interface{},bool) {
 	//return requestIDs[goID]
 }
 
+// Get 返回设置的 RequestID
+func GetReqIDForGoroutine() (interface{},bool) {
+	meta,ok := getReqMetaForGoroutine()
+	if ok {
+		return meta.(storeMeta).reqID, ok
+	}
+	return nil,ok
+}
+
+// Get 返回设置的 ReqContext
+func GetReqContextForGoroutine() (context.Context,bool) {
+	meta,ok := getReqMetaForGoroutine()
+	if ok {
+		return meta.(storeMeta).reqContext, ok
+	}
+	return nil,ok
+}
+
 // Delete 删除设置的 RequestID
-func DeleteReqIDForGoroutine() {
+func DeleteMetaForGoroutine() {
 	goID := getGoID()
 	rwm.Lock()
 	defer rwm.Unlock()
